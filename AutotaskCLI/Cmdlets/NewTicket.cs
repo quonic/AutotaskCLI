a0dd85221a3bffe9b76634e4ccd064be4905f841
Quonic <spyingwind@gmail.com>
2019-01-25 00:52:03 -0600

    Adds some string code for later

--- AutotaskCLI/Cmdlets/NewTicket.cs
+++ AutotaskCLI/Cmdlets/NewTicket.cs
@@ -6,8 +6,34 @@ namespace AutotaskCLI.Cmdlets
     [Cmdlet(VerbsCommon.New, "Ticket")]
     public class NewTicket : PSCmdlet
     {
+        [Parameter(
+            Mandatory = false)]
+        public string Title;
+
+        [Parameter(
+            Mandatory = false)]
+        public SwitchParameter OutXML;
+
         protected override void ProcessRecord()
         {
+            Autotask.Ticket newTicket = new Autotask.Ticket();
+            if(Title != null)
+            {
+                newTicket.Title = Title;
+            }
+        }
+        protected override void EndProcessing()
+        {
+            if(OutXML = true)
+            {
+                // TODO: convert NewTicket to XML
+            }
+            else
+            {
+                Autotask.Entity[] entity = new Autotask.Entity[1];
+                new Autotask.createRequest(AI, entity);
+            }
+            base.EndProcessing();
         }
     }
 }
